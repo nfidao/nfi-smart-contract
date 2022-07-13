e8ee6c1b15e13129295b8032f0259bff85f0967a
kenzeng0104 <kenzeng0104@gmail.com>
2022-07-13 14:17:55 +0700

    feature: add royalty registry to the flow

--- contracts/ModelNFTFactory.sol
+++ contracts/ModelNFTFactory.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.4;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
@@ -9,15 +9,10 @@ contract ModelNFTFactory is Initializable, OwnableUpgradeable {
     // Instantiate NFT contract
     ModelNFT private _modelNFT;
 
-    // treasury account
-    address payable private _royaltyReceiver;
-
     // Event
     event NFTCreated(string modelID, address modelNFTAddress);
 
-    function initialize(address receiver) public initializer {
-        _royaltyReceiver = payable(receiver);
-
+    function initialize() public initializer {
         __Ownable_init_unchained();
     }
 
@@ -25,7 +20,6 @@ contract ModelNFTFactory is Initializable, OwnableUpgradeable {
      * @dev Create new model NFT smart contract with parameters.
      * @param _modelName name of model.
      * @param _modelID ID of model.
-     * @param _rate royalty fee rate.
      * @param _designer Address of designer.
      * @param _manager Address of manager.
      * @param _signer Address of signer for minting.
@@ -34,40 +28,16 @@ contract ModelNFTFactory is Initializable, OwnableUpgradeable {
     function createModelNFT(
         string memory _modelName,
         string memory _modelID,
-        uint96 _rate,
         address _designer,
         address _manager,
         address _signer,
+        address _royaltyRegistry,
         uint256 _mintLimit
     ) external {
         require(_mintLimit > 0, "Incorrect mint limit.");
-        require(_rate < 1000, "Rate should be less than 1000.");
 
-        _modelNFT = new ModelNFT(
-            _modelName,
-            _modelID,
-            _mintLimit,
-            _rate,
-            _designer,
-            _manager,
-            _signer,
-            _royaltyReceiver
-        );
+        _modelNFT = new ModelNFT(_modelName, _modelID, _mintLimit, _designer, _manager, _signer, _royaltyRegistry);
 
         emit NFTCreated(_modelID, address(_modelNFT));
-    }
-
-    /**
-    @dev Change platform account
-    @param account wallet address or smart contract
-    */
-    function setRoyaltyReceiver(address account) external onlyOwner {
-        require(account != address(0), "Address can't be zero.");
-
-        _royaltyReceiver = payable(account);
-    }
-
-    function getRoyaltyReceiver() public view returns (address) {
-        return _royaltyReceiver;
     }
 }
