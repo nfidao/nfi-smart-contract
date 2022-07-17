1c2fca9779477f6e593a6a455c17b0b61c05bd1b
kenzeng0104 <kenzeng0104@gmail.com>
2022-07-17 15:24:11 +0700

    2/ deploy

--- deploy/ModelNFTFactory.ts
+++ deploy/ModelNFTFactory.ts
@@ -7,6 +7,7 @@ const func: DeployFunction = async ({
   network,
 }) => {
   const { deployer } = await getNamedAccounts();
+  const royaltyRegistry = "0x855e2Ed9B89B011f75439E6D47bC3924479BbA13";
 
   await deploy("ModelNFTFactory", {
     proxy: {
@@ -16,7 +17,7 @@ const func: DeployFunction = async ({
       execute: {
         init: {
           methodName: "initialize",
-          args: [],
+          args: [royaltyRegistry],
         },
       },
     },
